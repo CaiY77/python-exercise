db004289bd37029a595e1abb4c8962490a22161e
Cai Yang <caih.yang@gmail.com>
2019-06-20 16:22:01 -0400

    variables and methods

--- intro.py
+++ intro.py
@@ -1,1 +1,14 @@
 print ('hello world');
+
+# variables, tricks, functions
+
+someone = "Bob";
+print ('hello ' + someone);
+
+phrase = "This is just a long sentence"
+
+print (len(phrase))
+print (phrase.index('just'))
+
+number_array = ['one', 'two', 'three', 'four', 'five', 'six']
+print(number_array[1:3])
