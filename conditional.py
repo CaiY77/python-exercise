6618879bed72a0e4e264a46a23e81f859f3ab3c8
Cai Yang <caih.yang@gmail.com>
2019-06-22 16:02:26 -0400

    basic ifstatements

---
+++ conditional.py
@@ -0,0 +1,16 @@
+bool = True
+bool2 = False
+bool3 = True
+
+# if, else, elif
+
+if bool:
+    print("wow, so true")
+else:
+    print ("FALSE!")
+
+if bool or bool2:
+    print("something is true")
+
+if bool and bool3:
+    print("both are true")
