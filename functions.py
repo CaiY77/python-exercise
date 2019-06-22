d82db651bd5b21afe955f2ac08891ab509b9fc2b
Cai Yang <caih.yang@gmail.com>
2019-06-22 15:55:50 -0400

    function basics

---
+++ functions.py
@@ -0,0 +1,17 @@
+#basic function
+def sayHello():
+    print("Hello World")
+
+sayHello();
+
+#function with params
+def sayHelloTo(name):
+    print ("Hello " + name)
+
+sayHelloTo("Bob")
+
+#function with return
+def add(a , b):
+    return a+b;
+
+print(add(1,2))
