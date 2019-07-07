a7343a57f9e94691e456dded8770e709d83a28d1
Cai Yang <caih.yang@gmail.com>
2019-07-07 03:42:04 -0400

    cipher left shift excersize

--- loops.py
+++ loops.py
@@ -5,3 +5,14 @@ for i in range (10):
 mess = "This is a string"
 for c in range(0, len(mess), 2):
     print(mess[c])
+
+# does cipher left shift
+
+mess = "This is a random Message"
+newmessage=""
+for c in mess:
+    offset=ord(c)-ord("a")+25
+    wrap=offset%26
+    newchar=chr(ord("a")+wrap)
+    newmessage =newmessage+newchar
+print("Your new Message: ", newmessage)
